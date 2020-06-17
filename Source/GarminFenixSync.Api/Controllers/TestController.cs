995d8f3cd7e6ca42df2eb978179de71a5c0ddc1b
jmartinez <jose.martinez_fuentes@roche.com>
2020-06-17 16:59:41 +0200

    add publish to azure workflow

---
+++ Source/GarminFenixSync.Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GarminFenixSync.Api
+{
+    [Route("api/[Controller]")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            return this.Ok($"id requested: {id}");
+        }
+    }
+}
